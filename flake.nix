5f1e68bc11d37b81b50fd2b03bb4a6de78986423
John Shaffer <jdsha@pm.me>
2025-11-25 14:38:09 -0600

    Use stable zip hashes for plugin downloads

--- flake.nix
+++ flake.nix
@@ -18,7 +18,26 @@
   outputs =
     inputs:
     inputs.flake-utils.lib.eachDefaultSystem (
-      system: with import inputs.nixpkgs { inherit system; }; {
+      system:
+      with import inputs.nixpkgs { inherit system; };
+      let
+        # Fetch zips but take a stable hash of its contents, not the zip itself.
+        # Sometimes zip files change.
+        fetchStableZip =
+          { url, hash }:
+          let
+            unpacked = pkgs.fetchzip {
+              inherit url hash;
+              recursiveHash = true;
+              stripRoot = false;
+            };
+          in
+          pkgs.runCommand ((lib.nameFromURL url ".zip") + ".zip") { buildInputs = [ pkgs.zip ]; } ''
+            cd ${unpacked}
+            zip -r $out .
+          '';
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             fd
@@ -39,18 +58,18 @@
           '';
         };
         packages = {
-          litespeed-cache = fetchurl {
+          litespeed-cache = fetchStableZip {
             url = "https://downloads.wordpress.org/plugin/litespeed-cache.7.6.2.zip";
-            hash = "sha256-BVkpCYn+4+8z5QLnmeFsVS5zHWo/aG4eWmTC5mdxgnk=";
+            hash = "sha256-AduSCRqawoBvb0kVBDiM90mZ9naLhUcbPt3kcHoqlKU=";
           };
-          redis-cache = fetchurl {
+          redis-cache = fetchStableZip {
             url = "https://downloads.wordpress.org/plugin/redis-cache.2.7.0.zip";
-            hash = "sha256-DLxB3qNRaIaTuOfbEWW12StsjvcjGsD/Wr7HuD1ixjU=";
+            hash = "sha256-+BlAD31TBABJOeFvF7j7uelFO8nYfIkoKyhtIk38KpE=";
           };
           snapcache = inputs.snapcache.packages.${system}.pluginWpOrg;
-          wp-redis = fetchurl {
+          wp-redis = fetchStableZip {
             url = "https://downloads.wordpress.org/plugin/wp-redis.1.4.6.zip";
-            hash = "sha256-Cuw8ofc215EUMnVRVBxfwWxIg22cx8zKuJDgiKkZvLc=";
+            hash = "sha256-bB2xEF+w7o6mb1OzBPJPfnR1EFNNBfT/MkV6l0MrtA8=";
           };
         };
       }
